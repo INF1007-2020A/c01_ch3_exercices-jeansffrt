7b72ea4039ac2787ad652fdf06a0f94ab718582b
jeansffrt <jean.siffert.mtl@gmail.com>
2020-09-14 18:45:48 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -13,7 +13,7 @@ def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float
 
 def to_degrees(angle_rads: float) -> tuple:
     angle_degs = math.degrees(angle_rads)
-    angle_mins = math.modf()
+    angle_mins = math.modf(angl)
 
 def to_celsius(temperature: float) -> float:
     return (temperature - 32) / 1.8
